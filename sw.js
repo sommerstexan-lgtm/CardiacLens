cf53ebb146a73d42e20a320d7b40da1307a7eb14
sommerstexan-lgtm <sommers.texan@gmail.com>
2026-06-13 18:24:41 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'cardiaclens-v9.10.221';
+const CACHE_NAME = 'cardiaclens-v9.10.222';
 const urlsToCache = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png', './apple-touch-icon.png', './favicon-32.png', './favicon-16.png'];
 self.addEventListener('install', event => {
   self.skipWaiting();
