5dd794a1aff703d9918ae101ce1c3c25e5f39e57
sommerstexan-lgtm <sommers.texan@gmail.com>
2026-06-30 17:18:26 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'cardiaclens-v9.10.347.123-gps-update-readiness-repair';
+const CACHE_NAME = 'cardiaclens-v9.10.347.125-destination-state-repair';
 self.addEventListener('install', event => { self.skipWaiting(); });
 self.addEventListener('activate', event => { event.waitUntil(self.clients.claim()); });
 self.addEventListener('fetch', event => { event.respondWith(fetch(event.request).catch(() => caches.match(event.request))); });
