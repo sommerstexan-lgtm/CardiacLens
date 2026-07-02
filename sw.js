c4c5b5ef4bf92eb1bb7b206ddcb0df36bd2933ba
sommerstexan-lgtm <sommers.texan@gmail.com>
2026-07-02 17:33:47 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'cardiaclens-v9.10.347.139-gps-map-marker-stop-note-symptom-access';
+const CACHE_NAME = 'cardiaclens-v9.10.347.140-gps-pipeline-trace-first-fix-map-marker';
 self.addEventListener('install', event => { self.skipWaiting(); });
 self.addEventListener('activate', event => { event.waitUntil(self.clients.claim()); });
 self.addEventListener('fetch', event => {
