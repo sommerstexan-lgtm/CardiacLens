a54efd939b854aa4aa03232b91f3c13a22a75f1e
sommerstexan-lgtm <sommers.texan@gmail.com>
2026-07-24 12:34:33 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'cardiaclens-v9.10.347.198-fluid-push';
+const CACHE_NAME = 'cardiaclens-v9.10.347.199-fluid-push-race-fix';
 
 self.addEventListener('install', event => {
   self.skipWaiting();
