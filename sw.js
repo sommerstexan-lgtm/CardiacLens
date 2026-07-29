c1752343004a951c5a980135c2d77083e945702c
sommerstexan-lgtm <sommers.texan@gmail.com>
2026-07-29 14:59:24 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'cardiaclens-v9.10.347.212-hard-lock';
+const CACHE_NAME = 'cardiaclens-v9.10.347.213-lock-restore';
 
 self.addEventListener('install', event => {
   self.skipWaiting();
