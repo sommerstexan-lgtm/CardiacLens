6a274507aa9317815cf2837fe51130ad3a107eb0
sommerstexan-lgtm <sommers.texan@gmail.com>
2026-08-06 19:50:03 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'cardiaclens-v9.10.351.230-nosecui';
+const CACHE_NAME = 'cardiaclens-v9.10.351.231-nosecui';
 
 self.addEventListener('install', event => {
   self.skipWaiting();
